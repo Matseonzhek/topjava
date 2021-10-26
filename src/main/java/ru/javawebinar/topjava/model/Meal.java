86897be4160222ab2e0e0076f4ad3e8bc3faf4a4
Tanya Matseonzhek <matseonzhek@gmail.com>
2021-10-26 23:53:24 +0300

    HW03_task1-4_partOf_5

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -25,6 +25,8 @@ public class Meal extends AbstractBaseEntity {
         this.calories = calories;
     }
 
+    public Meal(Meal meal) { this(meal.id,meal.dateTime, meal.description,meal.calories);}
+
     public LocalDateTime getDateTime() {
         return dateTime;
     }
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,82 @@
+package ru.javawebinar.topjava.service;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.UserTestData.assertMatch;
+import static ru.javawebinar.topjava.web.user.MealTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    @Autowired
+    private MealService mealService;
+
+    @Test
+    public void get() {
+        Meal meal = mealService.get(MEAL_ID, USER_ID);
+        assertThat(meal).usingRecursiveComparison().isEqualTo(meals.get(0));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getOtherUserMeal() {
+        Meal meal = mealService.get(MEAL_ID, ADMIN_ID);
+        assertThat(meal).usingRecursiveComparison().isEqualTo(meals.get(0));
+    }
+
+    @Test
+    public void delete() {
+        mealService.delete(100002, USER_ID);
+        assertThrows(NotFoundException.class, ()->mealService.get(100000,USER_ID));
+    }
+
+
+    @Test(expected = NotFoundException.class)
+    public void deleteOtherUserMeal() {
+        mealService.delete(100002, ADMIN_ID);
+        assertThrows(NotFoundException.class, ()->mealService.get(100000,USER_ID));
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+        Meal updated = getUpdatedMeal();
+        mealService.update(updated,ADMIN_ID);
+        assertThat(mealService.get(100008,100001)).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(updated);
+    }
+
+    @Test
+    public void create() {
+        Meal created = mealService.create(getNewMeal(),USER_ID);
+        Integer newId = created.getId();
+        Meal newMeal = getNewMeal();
+        newMeal.setId(newId);
+        assertThat(created).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(newMeal);
+        assertThat(mealService.get(100009,USER_ID)).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(newMeal);
+
+    }
+}
